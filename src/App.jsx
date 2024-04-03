500dacfdb479d7bea8d8daca5340cc162ecde5e1
Melani Alassia <alassiamelani1@gmail.com>
2024-04-03 17:12:03 -0300

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,50 @@ function App() {
           <div className={style.banner}>
             <h1> CURSO DE MASTER EN CSS CON VÍCTOR ROBLES</h1>
           </div>
+          <div className={style.cards}>
+            <div className={style.card}>
+              <p className={style.icon}>H</p>
+              <h2 className={style.category}>Desarrollo Web</h2>
+              <p className={style.description}>
+                {" "}
+                Aprende los principales lenguajes para desarrollar webs
+                ¡Conviertete en un webmaster!
+              </p>
+            </div>
+            <div className={style.card}>
+              <p className={style.icon}>_</p>
+              <h2 className={style.category}>Sistemas Operativos</h2>
+              <p className={style.description}>
+                {" "}
+                Aprende a administrar sistemas Linux y Windows.
+              </p>
+            </div>
+            <div className={style.card}>
+              <p className={style.icon}>S</p>
+              <h2 className={style.category}>Hardware</h2>
+              <p className={style.description}>
+                {" "}
+                Conoce los entresijos del hardware.
+              </p>
+            </div>
+            <div className={style.card}>
+              <p className={style.icon}>u</p>
+              <h2 className={style.category}>Redes e Internet</h2>
+              <p className={style.description}>
+                {" "}
+                Aprende a configurar y administrar redes infromaticas y
+                servidores.
+              </p>
+            </div>
+            <div className={style.card}>
+              <p className={style.icon}>a</p>
+              <h2 className={style.category}>Bases de datos</h2>
+              <p className={style.description}>
+                {" "}
+                Aprende a montar y administrar bases de datos.
+              </p>
+            </div>
+          </div>
         </section>
       </section>
     </div>
